b82f558b8752409ed3c6c5896ea285a5275ecd24
adam.chen <adam.chen@medicustek.com>
2023-11-20 16:41:16 +0800

    auto collapse/un-collapse sider by screen width

--- src/layouts/responsiveLayout.js
+++ src/layouts/responsiveLayout.js
@@ -1,18 +1,25 @@
-import React from "react";
 import { useContext } from "react";
 import { StoreContext } from "../components/store/store";
 import { Actions } from "../components/store/reducer";
+import useWindowDimensions from "../utils/WindowDimension";
 import { Layout } from "antd";
+import { useEffect } from "react";
 
 const { Content, Sider } = Layout;
 export const SIDEBAR_WIDTH = 240;
 
 const ResponsiveLayout = ({ sidebar, main }) => {
+  const { width } = useWindowDimensions();
   const [store, dispatch] = useContext(StoreContext);
 
   const onCollapse = (collapsed) => {
     dispatch({ type: Actions.SetSiderCollapse, payload: collapsed });
   };
+
+  useEffect(() => {
+    if (width > 1024) onCollapse(false);
+    else onCollapse(true);
+  }, [width]);
 
   return (
     <Layout style={{ minHeight: "100vh" }}>
@@ -21,6 +28,7 @@ const ResponsiveLayout = ({ sidebar, main }) => {
         theme="light"
         width={SIDEBAR_WIDTH}
         collapsible
+        collapsed={store.siderCollapse}
         onCollapse={(collapsed, type) => {
           onCollapse(collapsed);
         }}
