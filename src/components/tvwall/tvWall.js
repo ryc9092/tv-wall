b8f07fc92202e979a37ee1556ea34b9cc8e5ebec
Leslie PC Chang (張沛晴) <LesliePC_Chang@moxa.com>
2023-12-17 23:46:45 +0800

    finish tv wall template blocks layout

--- src/components/tvwall/tvWall.js
+++ src/components/tvwall/tvWall.js
@@ -13,78 +13,25 @@ import {
   Typography,
 } from "antd";
 import { EditOutlined } from "@ant-design/icons";
-import { FAKE_WALLS, blockColorList } from "../../utils/Constant";
+import {
+  fakewall1,
+  fakewall2,
+  fakewall3,
+  fakewall4,
+  FAKE_WALLS,
+  blockColorList,
+} from "../../utils/Constant";
 import "../../App.scss";
 import "./tvWall.scss";
 
-const fakewall = [
-  {
-    number: 1,
-    decoder: "解碼器1",
-    block: 1,
-  },
-  {
-    number: 2,
-    decoder: "解碼器2",
-    block: 1,
-  },
-  {
-    number: 3,
-    decoder: "解碼器3",
-    block: 1,
-  },
-  {
-    number: 4,
-    decoder: "解碼器4",
-    block: 2,
-  },
-  {
-    number: 5,
-    decoder: "解碼器5",
-    block: 1,
-  },
-  {
-    number: 6,
-    decoder: "解碼器6",
-    block: 1,
-  },
-  {
-    number: 7,
-    decoder: "解碼器7",
-    block: 1,
-  },
-  {
-    number: 8,
-    decoder: "解碼器8",
-    block: 3,
-  },
-  {
-    number: 9,
-    decoder: "解碼器61",
-    block: 1,
-  },
-  {
-    number: 10,
-    decoder: "解碼器71",
-    block: 1,
-  },
-  {
-    number: 11,
-    decoder: "解碼器81",
-    block: 1,
-  },
-  {
-    number: 12,
-    decoder: "解碼器611",
-    block: 4,
-  },
-];
-
 const TvWall = () => {
-  const [tvWallSize, setTvWallSize] = useState({ col: 4, row: 3 });
+  const [tvWallSize, setTvWallSize] = useState({ col: 4, row: 5 });
   const [tvWallTemplate, setTvWallTemplate] = useState(null);
   const [blocks, setBlocks] = useState([]);
-  const screenSizeUnit = 40;
+  const [blocksWithPosition, setBlocksWithPosition] = useState([]);
+  const [blocksByHtml, setBlocksByHtml] = useState([]);
+  const screenSizeUnit = { horizontal: 60, straight: 40 };
+  const fakewall = fakewall4;
 
   const getAboveScreen = (screen) => {
     if (screen.number > tvWallSize.col) {
@@ -128,6 +75,7 @@ const TvWall = () => {
           row: 1,
           block: block,
           decoder: [decoder],
+          marginLeft: (screen.number - 1) % tvWallSize.col,
         });
       }
     });
@@ -135,227 +83,79 @@ const TvWall = () => {
   }, [fakewall]);
 
   useEffect(() => {
-    // create tv wall template
-    let wall = [];
+    // create blocks list with margin top info
     let currentColInRows = {};
-    for (let i = 1; i <= tvWallSize.row; i++) {
+    for (let i = 0; i < tvWallSize.row; i++) {
       currentColInRows[i] = 0;
     }
 
-    // let currentCol = 0;
-    // let currentRow = 0;
-    blocks.forEach((block) => {
-      // add to this row
-      // if (currentCol < tvWallSize.col) {
-      //   // margin =
-      //   // add to next row
-      // } else {
-      // }
+    let marginTop = 0;
+    let tempBlocks = blocks;
+    blocks.forEach((block, index) => {
+      // for row in rows
+      for (const [row, col] of Object.entries(currentColInRows)) {
+        // if row.col < tv wall col
+        if (col < tvWallSize.col) {
+          // set margin top information
+          tempBlocks[index].marginTop = parseInt(marginTop);
+          if (marginTop !== 0) marginTop = 0;
+          // update used row & col information
+          for (let i = 0; i < block.row; i++) {
+            if (
+              currentColInRows[parseInt(row) + i] + block.col <
+              tvWallSize.col
+            )
+              marginTop = marginTop - 1;
+            currentColInRows[parseInt(row) + i] =
+              currentColInRows[parseInt(row) + i] + block.col;
+          }
+          break;
+        }
+      }
+    });
+    setBlocksWithPosition(tempBlocks);
+  }, [blocks]);
 
+  useEffect(() => {
+    let wall = [];
+    let currentAppearedRow = 0;
+    blocksWithPosition.forEach((block, index) => {
       wall.push(
         <div
           key={block.block}
           style={{
-            width: screenSizeUnit * block.col,
-            height: screenSizeUnit * block.row,
+            width: screenSizeUnit.horizontal * block.col,
+            height: screenSizeUnit.straight * block.row,
+            marginLeft: screenSizeUnit.horizontal * block.marginLeft,
+            marginTop: screenSizeUnit.straight * block.marginTop,
             border: "1px dashed black",
           }}
         >
-          {block.block}
+          <Button
+            style={{ width: "100%", height: "100%" }}
+            onClick={setVideoToBlock(index)}
+          >
+            <img
+              style={{ width: "100%", height: "100%" }}
+              src={block.video}
+            ></img>
+          </Button>
         </div>
       );
+
+      currentAppearedRow = currentAppearedRow + block.row;
     });
     setTvWallTemplate(<div>{wall}</div>);
-  }, [blocks]);
+  }, [blocksWithPosition]);
 
-  return (
-    <div>
-      {/* <div
-        style={{
-          width: screenSizeUnit * tvWallSize.col,
-          height: screenSizeUnit * tvWallSize.row,
-          border: "1px solid black",
-          display: "flex",
-          flexWrap: "wrap",
-        }}
-      >
-        <div
-          style={{
-            width: 79,
-            height: 79,
-            border: "1px dashed black",
-            display: "inline-block",
-          }}
-        >
-          1
-        </div>
-        <div
-          style={{
-            width: 79,
-            height: 39,
-            border: "1px dashed black",
-            display: "inline-block",
-          }}
-        >
-          2
-        </div>
-        <div
-          style={{
-            width: 79,
-            height: 39,
-            border: "1px dashed black",
-            display: "inline-block",
-          }}
-        >
-          3
-        </div>
-        <div
-          style={{
-            width: 79,
-            height: 79,
-            marginTop: -40,
-            border: "1px dashed black",
-            display: "inline-block",
-          }}
-        >
-          4
-        </div> */}
-      {/* <div
-          style={{
-            width: 79,
-            height: 39,
-            marginTop: -40,
-            border: "1px dashed black",
-            display: "inline-block",
-          }}
-        >
-          5
-        </div>
-      </div> */}
-      {tvWallTemplate}
+  const setVideoToBlock = (blockIdx) => {
+    //https://www.gstatic.com/webp/gallery3/2.png
+    let tempBlocks = blocksWithPosition;
+    tempBlocks[blockIdx].video = "https://www.gstatic.com/webp/gallery3/2.png";
+    setBlocks(tempBlocks);
+  };
 
-      {/* <table>
-        <thead>
-          <tr>
-            <th colSpan="2" rowSpan="3">
-              test
-            </th>
-            <th colSpan="2" rowSpan="2">
-              test
-            </th>
-            <th>test</th>
-            <td>test</td>
-            <th>test</th>
-          </tr>
-          <tr>
-            <th colSpan="2" rowSpan="3">
-              test
-            </th>
-            <th>test</th>
-            <th>test</th>
-            <th>test</th>
-          </tr>
-          <tr>
-            <th>test</th>
-            <th>test</th>
-            <th>test</th>
-            <th>test</th>
-          </tr>
-          <tr>
-            <th>test</th>
-            <th>test</th>
-            <th>test</th>
-            <th>test</th>
-          </tr>
-        </thead>
-      </table> */}
-
-      {/* <table>
-        <tbody>
-          <tr>
-            <td colSpan="2" rowSpan="3">
-              test
-            </td>
-            <td colSpan="2" rowSpan="2">
-              test
-            </td>
-          </tr>
-          <tr>
-            <td colSpan="2" rowSpan="3">
-              test
-            </td>
-            <td colSpan="2" rowSpan="4">
-              test
-            </td>
-          </tr>
-        </tbody>
-      </table> */}
-
-      {/* <table
-        border="1"
-        cellSpacing="5"
-        cellPadding="5"
-        summary="This table combines colspan and rowspan"
-        style={{ margin: 12 }}
-      >
-        <tbody>
-          <tr>
-            <td colSpan="2" rowSpan="2">
-              data
-            </td>
-            <td>data</td>
-          </tr>
-          <tr>
-            <td>data</td>
-          </tr>
-          <tr>
-            <td>data</td>
-            <td>data</td>
-            <td>data</td>
-          </tr>
-          <tr>
-            <td>data</td>
-            <td>data</td>
-            <td>data</td>
-          </tr>
-        </tbody>
-      </table> */}
-
-      {/* <table
-        border="1"
-        cellSpacing="5"
-        cellPadding="5"
-        summary="This table combines colspan and rowspan"
-        style={{ margin: 12 }}
-      >
-        <tbody>
-          <tr>
-            <td colSpan="2" rowSpan="2">
-              data
-            </td>
-            <td colSpan="2" rowSpan="3">
-              data
-            </td>
-          </tr>
-          <tr>
-            <td>1</td>
-          </tr>
-          <tr>
-            <td>data</td>
-            <td>data</td>
-            <td>data</td>
-            <td>data</td>
-          </tr>
-          <tr>
-            <td>data</td>
-            <td>data</td>
-            <td>data</td>
-            <td>data</td>
-          </tr>
-        </tbody>
-      </table> */}
-    </div>
-  );
+  return <div>{tvWallTemplate}</div>;
 };
 
 export default TvWall;
