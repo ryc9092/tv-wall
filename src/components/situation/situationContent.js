36c31e2b3c45bb9b668f35405f985150c329a201
adam.chen <adam.chen@medicustek.com>
2024-03-27 15:00:10 +0800

    make situation content dragable

--- src/components/situation/situationContent.js
+++ src/components/situation/situationContent.js
@@ -1,35 +1,129 @@
-import { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Button, Modal, Table, Typography } from "antd";
+import { MenuOutlined } from "@ant-design/icons";
+import { DndContext } from "@dnd-kit/core";
+import { restrictToVerticalAxis } from "@dnd-kit/modifiers";
+import {
+  arrayMove,
+  SortableContext,
+  useSortable,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 import AddSituationContentModal from "./addSituationContent";
 import "./createSituation.scss";
 
+const columns = [
+  {
+    key: "sort",
+  },
+  {
+    title: "order",
+    dataIndex: "order",
+  },
+  {
+    title: "type",
+    dataIndex: "type",
+  },
+  {
+    title: "desc",
+    dataIndex: "desc",
+  },
+  {
+    title: "operation",
+    dataIndex: "operation",
+  },
+];
+const Row = ({ children, ...props }) => {
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    setActivatorNodeRef,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({
+    id: props["data-row-key"],
+  });
+  const style = {
+    ...props.style,
+    transform: CSS.Transform.toString(
+      transform && {
+        ...transform,
+        scaleY: 1,
+      }
+    ),
+    transition,
+    ...(isDragging
+      ? {
+          position: "relative",
+          zIndex: 9999,
+        }
+      : {}),
+  };
+  return (
+    <tr {...props} ref={setNodeRef} style={style} {...attributes}>
+      {React.Children.map(children, (child) => {
+        if (child.key === "sort") {
+          return React.cloneElement(child, {
+            children: (
+              <MenuOutlined
+                ref={setActivatorNodeRef}
+                style={{
+                  touchAction: "none",
+                  cursor: "move",
+                }}
+                {...listeners}
+              />
+            ),
+          });
+        }
+        return child;
+      })}
+    </tr>
+  );
+};
+
 const SituationContentModal = ({ id, name, desc }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [dataSource, setDataSource] = useState([
+    {
+      key: "1",
+      order: "1",
+      type: "John Brown",
+      operation: 32,
+      desc: "Long text Long text Long text Long text Long text Long text Long text Long ",
+    },
+    {
+      key: "2",
+      order: "2",
+      type: "Jim Green",
+      operation: 42,
+      desc: "London No. 1 Lake Park",
+    },
+    {
+      key: "3",
+      order: "3",
+      type: "Joe Black",
+      operation: 32,
+      desc: "Sidney No. 1 Lake Park",
+    },
+  ]);
 
-  const data = [];
+  useEffect(() => {
+    console.log(dataSource);
+  }, [dataSource]);
 
-  const columns = [
-    {
-      title: "order", // todo: antd table -> Drag sorting with handler
-      dataIndex: "type",
-      key: "type",
-    },
-    {
-      title: "type",
-      dataIndex: "type",
-      key: "type",
-    },
-    {
-      title: "desc",
-      dataIndex: "type",
-      key: "type",
-    },
-    {
-      title: "operation",
-      dataIndex: "type",
-      key: "type",
-    },
-  ];
+  const onDragEnd = ({ active, over }) => {
+    if (active.id !== over?.id) {
+      setDataSource((previous) => {
+        const activeIndex = previous.findIndex((i) => i.key === active.id);
+        const overIndex = previous.findIndex((i) => i.key === over?.id);
+        return arrayMove(previous, activeIndex, overIndex);
+      });
+    }
+  };
 
   return (
     <div>
@@ -51,7 +145,24 @@ const SituationContentModal = ({ id, name, desc }) => {
         }}
       >
         <AddSituationContentModal id={id} />
-        <Table columns={columns} dataSource={data} />
+        <DndContext modifiers={[restrictToVerticalAxis]} onDragEnd={onDragEnd}>
+          <SortableContext
+            // rowKey array
+            items={dataSource.map((i) => i.key)}
+            strategy={verticalListSortingStrategy}
+          >
+            <Table
+              components={{
+                body: {
+                  row: Row,
+                },
+              }}
+              rowKey="key"
+              columns={columns}
+              dataSource={dataSource}
+            />
+          </SortableContext>
+        </DndContext>
       </Modal>
     </div>
   );
