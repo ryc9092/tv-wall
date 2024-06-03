c22065c4cfab997a7bb9475582b57be5e06d01b1
adam.chen <adam.chen@medicustek.com>
2024-06-03 18:33:08 +0800

    update single screen page

--- src/pages/SingleScreen.js
+++ src/pages/SingleScreen.js
@@ -1,10 +1,9 @@
 import React, { useContext, useEffect, useState } from "react";
 import { StoreContext } from "../components/store/store";
-import { Button, Card, Col, Input, Radio, Row, Select, Switch, Tabs, Tag, Table, Typography } from "antd";
+import { Card, Col, Input, Row, Tabs, Tag, Table } from "antd";
 import { SearchOutlined } from "@ant-design/icons";
 import useWindowDimensions from "../utils/WindowDimension";
-import { ENCODER_TYPERS, FAKE_ENCODERS } from "../utils/Constant";
-import { getEncoders } from "../api/API";
+import { getDecoders, getEncoders } from "../api/API";
 import { FormattedMessage, useIntl } from "react-intl";
 import Messages from "../messages";
 import "../App.scss";
@@ -14,39 +13,29 @@ const SingleScreen = () => {
   const intl = useIntl();
   const { height, width } = useWindowDimensions();
   const [store] = useContext(StoreContext);
+  const [tab, setTab] = useState("single-screen");
+  const [decoders, setDecoders] = useState([]);
+  const [searchDecoderFilter, setSearchDecoderFilter] = useState("");
+  const [filteredDecoders, setFilteredDecoders] = useState([]);
+  const [decoderCards, setDecoderCards] = useState(null);
   const [encoders, setEncoders] = useState([]);
-  const [encoderType, setEncoderType] = useState("1");
   const [searchFilter, setSearchFilter] = useState("");
   const [filteredEncoders, setFilteredEncoders] = useState([]);
   const [selectedEncoder, setSelectedEncoder] = useState({
     name: "",
     previewUrl: "",
   });
-  const [isSmallElement, setIsSmallElement] = useState(false);
 
   const tabItems = [
     {
-      key: '1',
-      label: '單顯示器',
-      children: 'Content of Tab Pane 1',
+      key: "single-screen",
+      label: "單顯示器",
+      // children: <Row style={{ width: "100%" }}>{decoderCards}</Row>,
+      onClick: () => {
+        setTab("single-screen");
+      },
     },
   ];
-
-  // The elements size would be changed according to width
-  useEffect(() => {
-    if (width > 960 && isSmallElement) setIsSmallElement(false);
-    else if (width < 960 && !isSmallElement) setIsSmallElement(true);
-  }, [isSmallElement, width]);
-
-  let encoderElement = [];
-  FAKE_ENCODERS.forEach((encoder) => {
-    encoderElement.push(
-      <Row key={encoder} style={{ marginTop: "10px" }}>
-        <span className="encoder-normal-dot" />
-        {encoder}
-      </Row>
-    );
-  });
 
   // Set "normal/abnormal encoder list" when search filter is changed
   useEffect(() => {
@@ -63,55 +52,92 @@ const SingleScreen = () => {
     })();
   }, [searchFilter]);
 
-  const handleChooseEncoder = (event) => {
+  const handleChooseEncoder = (encoder) => {
     setSelectedEncoder({
-      name: event.currentTarget.id,
-      previewUrl: event.currentTarget.value,
+      mac: encoder.mac,
+      previewUrl: encoder.previewUrl,
+      nickName: encoder.nickName,
     });
   };
 
-  const changeEncoderType = ({ target: { value } }) => {
-    setEncoderType(value);
-  };
+  // Set "normal/abnormal encoder list" when search filter is changed
+  useEffect(() => {
+    (async () => {
+      let tempFilteredDecoders = [];
+      const decoders = await getDecoders(store);
+      setDecoders(decoders);
+      decoders.forEach((decoder) => {
+        if (decoder.nickName.includes(searchDecoderFilter))
+          tempFilteredDecoders.push(decoder);
+      });
+      setFilteredDecoders(tempFilteredDecoders);
+    })();
+  }, [searchDecoderFilter]);
 
-  let encoderScreen = [];
-  FAKE_ENCODERS.forEach((encoder) => {
-    encoderScreen.push(
-      <div
-        key={`${encoder}-screen`}
-        style={{
-          margin: "12px 6px 0px 0px",
-          height: "48%",
-          width: "48%",
-          textAlign: "center",
-        }}
-      >
-        <div
-          key={`${encoder}-screen`}
-          style={{
-            height: "82%",
-            border: "1px solid gray",
-            marginBottom: "2px",
-          }}
-        >
-          {encoder}
-        </div>
-        <span style={{ textAlign: "center" }}>
-          <Typography.Text style={{ fontSize: "14px", marginRight: "6px" }}>
-            {encoder}
-          </Typography.Text>
-          <Switch style={{ marginTop: "-2px" }} />
-        </span>
-      </div>
-    );
-  });
+  useEffect(() => {
+    let tempDecoderCards = [];
+    filteredDecoders.forEach((decoder) => {
+      tempDecoderCards.push(
+        <Col span={6}>
+          <Card className="single-screen-card">
+            <div className="single-screen-card-title-row">
+              <span className="single-screen-card-title">
+                {decoder.nickName}
+              </span>
+              <span style={{ marginTop: "6px" }}>
+                {decoder.state === "Up" ? (
+                  <Tag
+                    color={"#eef9b4"}
+                    key={`${decoder.name}.${decoder.state}`}
+                  >
+                    <span style={{ color: "#a0b628" }}>
+                      <FormattedMessage {...Messages.Text_Common_Up} />
+                    </span>
+                  </Tag>
+                ) : decoder.state === "Down" ? (
+                  <Tag
+                    color={"#ffe6e5"}
+                    key={`${decoder.name}.${decoder.state}`}
+                  >
+                    <span style={{ color: "#d55959" }}>
+                      <FormattedMessage {...Messages.Text_Common_Down} />
+                    </span>
+                  </Tag>
+                ) : (
+                  <Tag
+                    color={"yellow"}
+                    key={`${decoder.name}.${decoder.state}`}
+                  >
+                    {decoder.state}
+                  </Tag>
+                )}
+              </span>
+            </div>
+          </Card>
+        </Col>
+      );
+    });
+    setDecoderCards(tempDecoderCards);
+  }, [filteredDecoders]);
 
   const columns = [
     {
       title: intl.formatMessage(Messages.Text_Common_Name),
       dataIndex: "nickName",
       key: "nickName",
-      render: (text) => text,
+      render: (text) => {
+        return (
+          <span
+            style={
+              selectedEncoder.nickName === text
+                ? { backgroundColor: "#FDEBD0" }
+                : null
+            }
+          >
+            {text}
+          </span>
+        );
+      },
     },
     {
       title: intl.formatMessage(Messages.Text_Common_Model),
@@ -165,82 +191,88 @@ const SingleScreen = () => {
           <span className="page-title">
             <FormattedMessage {...Messages.Text_SingleScreen_Management} />
           </span>
-            <Input className="single-screen-title-input singlescreen-input"
-              variant="filled"
-              prefix={<SearchOutlined />}
-              placeholder={intl.formatMessage(Messages.Text_SingleScreen_InputDecoder)}
-            />
+          <Input
+            className="single-screen-title-input singlescreen-input"
+            variant="filled"
+            onChange={(e) => {
+              setSearchDecoderFilter(e.target.value);
+            }}
+            prefix={<SearchOutlined />}
+            placeholder={intl.formatMessage(
+              Messages.Text_SingleScreen_InputDecoder
+            )}
+          />
         </div>
-        <div className="single-screen-title-row" style={{marginTop: "16px"}}>
+        <div className="single-screen-title-row" style={{ marginTop: "16px" }}>
           <Tabs defaultActiveKey="1" items={tabItems} />
-          <div>
-            clear
-          </div>
+          {/* <div>clear</div> */}
         </div>
         <div>
-          screens
+          <Row gutter={10}>{tab === "single-screen" ? decoderCards : null}</Row>
         </div>
       </div>
       <div
-          className="singlescreen-card-container"
-          style={{ minHeight: `${height - 8}px` }} // deduct topbar & padding
-        >
-          <Card className="singlescreen-card-right">
-            <div className="singlescreen-card-right-title">
-              <FormattedMessage {...Messages.Text_TVWall_VideoSource} />
-            </div>
-            <div className="singlescreen-card-right-desc">
-              <FormattedMessage {...Messages.Text_TVWall_VideoSourceDesc} />
-            </div>
-            <div className="singlescreen-card-right-preview">
-              {selectedEncoder.previewUrl ? (
-                <div>
-                  <embed
-                    className="singlescreen-card-right-preview-video"
-                    src={selectedEncoder.previewUrl}
-                    title="Video player"
-                  />
-                  <span>{selectedEncoder.nickName}</span>
-                </div>
-              ) : (
-                <div className="singlescreen-card-right-preview-text singlescreen-card-right-desc">
-                  <FormattedMessage {...Messages.Text_TVWall_Preview} />
-                </div>
-              )}
-            </div>
-            <Input
-              className="singlescreen-card-right-search singlescreen-input"
-              variant="filled"
-              onChange={(e) => {
-                setSearchFilter(e.target.value);
-              }}
-              prefix={<SearchOutlined />}
-              placeholder={intl.formatMessage(Messages.Text_TVWall_InputEncoder)}
-            />
-            <div className="singlescreen-card-right-encoder-container">
-              <Table
-                columns={columns}
-                dataSource={filteredEncoders}
-                pagination={false}
-                size={"small"}
-                onRow={(record) => {
-                  return {
-                    onClick: () => {
-                      handleChooseEncoder(record);
-                    },
-                  };
-                }}
-                style={{ maxHeight: `${height - 510}px`, marginBottom: 0 }}
-              />
-            </div>
-            <div className="singlescreen-card-right-bottom">
-              <div className="singlescreen-card-right-bottom-text">
-                <FormattedMessage {...Messages.Text_SingleScreen_ConnectType} />
+        className="tvwall-card-container"
+        style={{ minHeight: `${height - 104}px` }} // deduct topbar & padding
+      >
+        {/* <div
+        className="singlescreen-card-container"
+        style={{ height: "fit-content" }} // for connection type
+      > */}
+        <Card className="singlescreen-card-right">
+          <div className="singlescreen-card-right-title">
+            <FormattedMessage {...Messages.Text_TVWall_VideoSource} />
+          </div>
+          <div className="singlescreen-card-right-desc">
+            <FormattedMessage {...Messages.Text_TVWall_VideoSourceDesc} />
+          </div>
+          <div className="singlescreen-card-right-preview">
+            {selectedEncoder.previewUrl ? (
+              <div>
+                <embed
+                  className="singlescreen-card-right-preview-video"
+                  src={selectedEncoder.previewUrl}
+                  title="Video player"
+                />
+                <span>{selectedEncoder.nickName}</span>
               </div>
-                <Select className="singlescreen-card-right-bottom-select" />
+            ) : (
+              <div className="singlescreen-card-right-preview-text singlescreen-card-right-desc">
+                <FormattedMessage {...Messages.Text_TVWall_Preview} />
+              </div>
+            )}
+          </div>
+          <Input
+            className="singlescreen-card-right-search singlescreen-input"
+            variant="filled"
+            onChange={(e) => {
+              setSearchFilter(e.target.value);
+            }}
+            prefix={<SearchOutlined />}
+            placeholder={intl.formatMessage(Messages.Text_TVWall_InputEncoder)}
+          />
+          <div className="singlescreen-card-right-encoder-container">
+            <Table
+              columns={columns}
+              dataSource={filteredEncoders}
+              pagination={false}
+              size={"small"}
+              onRow={(record) => ({
+                onClick: () => {
+                  handleChooseEncoder(record);
+                },
+              })}
+              style={{ maxHeight: `${height - 510}px`, marginBottom: 0 }}
+            />
+          </div>
+          {/* <div className="singlescreen-card-right-bottom">
+            <div className="singlescreen-card-right-bottom-text">
+              <FormattedMessage {...Messages.Text_SingleScreen_ConnectType} />
             </div>
-          </Card>
-        </div>
+            <Select className="singlescreen-card-right-bottom-select" />
+          </div> */}
+        </Card>
+      </div>
     </div>
   );
 };
