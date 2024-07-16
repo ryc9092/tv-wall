e86c2ab6fc4d5fdd89d924153a6f96c357d98677
adam.chen <adam.chen@medicustek.com>
2024-07-16 18:57:33 +0800

    add situation list

--- src/pages/Situation.js
+++ src/pages/Situation.js
@@ -1,12 +1,21 @@
 import React, { useContext, useEffect, useState } from "react";
-import { Button, Card } from "antd";
+import { Button, Card, Table } from "antd";
 import { StoreContext } from "../components/store/store";
 import { DeleteOutlined } from "@ant-design/icons";
 import { FormattedMessage, useIntl } from "react-intl";
 import Messages from "../messages";
 import CreateSituation from "../components/situation/createSituation";
 import SituationContentModal from "../components/situation/situationContent";
-import { getSituations, removeSituation, activateSituation } from "../api/API";
+import {
+  getSituations,
+  getSituationDetails,
+  removeSituation,
+  activateSituation,
+} from "../api/API";
+import PlayIcon from "../assets/play-black.png";
+import CaretIcon from "../assets/caret-down.png";
+import TrashIcon from "../assets/trash.png";
+import PlusYellowIcon from "../assets/plus-yellow.png";
 import "../App.scss";
 import "./Situation.scss";
 
@@ -16,6 +25,8 @@ const Situation = () => {
   const [situationCards, setSituationCards] = useState([]);
   const [situationActivated, setSituationActivated] = useState([]);
   const [reload, setReload] = useState(null);
+  const [expandSituation, setExpandSituation] = useState(null);
+  const [situationItems, setSituationItems] = useState([]);
 
   const startSituation = async (situationId) => {
     activateSituation(situationId, store).then(() => {
@@ -32,42 +43,119 @@ const Situation = () => {
   useEffect(() => {
     (async () => {
       const situations = await getSituations(store);
+
       if (situations) {
-        let cards = [];
-        situations.forEach((situation) => {
-          cards.push(
-            <Card
-              title={
-                <>
-                  <span style={{ marginRight: 10 }}>{situation.name}</span>{" "}
+        let situation = situations[0];
+        console.log(situation, "!!!");
+        let situationDetail = await getSituationDetails(store, situation.id);
+        setSituationItems(situationDetail);
+        console.log(situationDetail, "kdj;flskdjflk");
+        let expandSituationCard = (
+          <Card
+            title={
+              <div className="situation-card-title-row">
+                <div className="situation-card-title">{situation.name}</div>
+                <div>
                   <Button
-                    disabled={situationActivated.includes(situation.id)}
+                    type="text"
+                    className="situation-card-play-btn"
                     onClick={() => startSituation(situation.id)}
                   >
-                    <FormattedMessage {...Messages.Text_Situation_Activate} />
+                    <img
+                      alt="play"
+                      src={PlayIcon}
+                      className="situation-card-play-icon"
+                    />
                   </Button>
-                </>
-              }
-              key={situation.id}
-              className="situation-card"
-              extra={
-                <Button type="text" id={situation.id} onClick={deleteSituation}>
-                  <DeleteOutlined />
-                </Button>
-              }
-            >
-              <div style={{ marginLeft: 54, marginTop: 0 }}>
-                <SituationContentModal
-                  id={situation.id}
-                  name={situation.name}
-                  desc={situation.remark}
-                />
+                  <Button
+                    type="text"
+                    className="situation-card-caret-btn"
+                    onClick={() => {}}
+                    style={{ marginLeft: 12 }}
+                  >
+                    <img
+                      alt="caret"
+                      src={CaretIcon}
+                      className="situation-card-caret-icon"
+                    />
+                  </Button>
+                </div>
               </div>
-            </Card>
-          );
-        });
-        setSituationCards(cards);
+            }
+            key={situation.id}
+            className="situation-card-expanded"
+            // extra={
+            //   <Button type="text" id={situation.id} onClick={deleteSituation}>
+            //     <DeleteOutlined />
+            //   </Button>
+            // }
+          >
+            <div className="situation-card-content">
+              <div className="situation-description">{situation.remark}</div>
+              <Table
+                columns={columns}
+                dataSource={situationDetail}
+                className={
+                  situationDetail.length === 0 ? "table-no-item" : null
+                }
+                pagination={false}
+              />
+              <Button
+                type="text"
+                className="add-situation-item-btn"
+                onClick={() => {}}
+              >
+                <img
+                  alt="create"
+                  src={PlusYellowIcon}
+                  className="add-situation-item-btn-icon"
+                />
+                <span className="add-situation-item-btn-text">
+                  <FormattedMessage
+                    {...Messages.Text_Situation_AddSituationItem}
+                  />
+                </span>
+              </Button>
+            </div>
+          </Card>
+        );
+        setExpandSituation(expandSituationCard);
       }
+
+      let cards = [];
+      situations?.forEach((situation) => {
+        cards.push(
+          <Card
+            title={
+              <>
+                <span style={{ marginRight: 10 }}>{situation.name}</span>{" "}
+                <Button
+                  disabled={situationActivated.includes(situation.id)}
+                  onClick={() => startSituation(situation.id)}
+                >
+                  <FormattedMessage {...Messages.Text_Situation_Activate} />
+                </Button>
+              </>
+            }
+            key={situation.id}
+            className="situation-card-normal"
+            extra={
+              <Button type="text" id={situation.id} onClick={deleteSituation}>
+                <DeleteOutlined />
+              </Button>
+            }
+          >
+            <div>
+              <SituationContentModal
+                id={situation.id}
+                name={situation.name}
+                desc={situation.remark}
+              />
+            </div>
+          </Card>
+        );
+      });
+      setSituationCards(cards);
     })();
   }, [reload]);
 
@@ -77,9 +165,74 @@ const Situation = () => {
     setReload(Math.random());
   };
 
+  const columns = [
+    {
+      title: (
+        <span className="table-head">
+          {intl.formatMessage(Messages.Text_Situation_Type)}
+        </span>
+      ),
+      dataIndex: "set_type",
+      key: "type",
+      render: (text) => <span className="table-content">{text}</span>,
+    },
+    {
+      title: (
+        <span className="table-head">
+          {intl.formatMessage(Messages.Text_Common_Name)}
+        </span>
+      ),
+      dataIndex: "id",
+      key: "name",
+      render: (text) => {
+        return <span className="table-content">{text}</span>;
+      },
+    },
+    {
+      title: (
+        <span className="table-head">
+          {intl.formatMessage(Messages.Text_Common_Description)}
+        </span>
+      ),
+      dataIndex: "remark",
+      key: "description",
+      render: (text) => {
+        return <span className="table-content">{text}</span>;
+      },
+    },
+    {
+      title: (
+        <span className="table-head">
+          {intl.formatMessage(Messages.Text_Button_Operation)}
+        </span>
+      ),
+      dataIndex: "id",
+      key: "operate",
+      render: (text, record) => {
+        return (
+          <div key={`${text}-action`}>
+            <Button
+              key={`${text}-delete`}
+              id={text}
+              type="text"
+              onClick={() => {}}
+              className="table-content"
+            >
+              <img
+                alt="remove"
+                src={TrashIcon}
+                className="table-content-icon"
+              />
+            </Button>
+          </div>
+        );
+      },
+    },
+  ];
+
   return (
     <div>
-      <div className="situation-layout-column">
+      <div className="situation-title-layout">
         <div className="situation-title-row">
           <div className="page-title">
             <FormattedMessage {...Messages.Text_Situation_Title} />
@@ -87,7 +240,11 @@ const Situation = () => {
           <CreateSituation setReload={setReload} />
         </div>
       </div>
-      {/* <div style={{ marginTop: 68 }}>{situationCards}</div> */}
+      <div className="situation-list-layout">
+        <div>{expandSituation}</div>
+        <div>{situationCards}</div>
+        {/* <div>{situationCards}</div> */}
+      </div>
     </div>
   );
 };
